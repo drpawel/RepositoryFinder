625bb1e1f69afb716f6353c8af0c9618507b4e93
Paweł Drewek <p.drewek@stud.elka.pw.edu.pl>
2021-04-24 02:37:20 +0200

    Added AppService functions

--- src/main/java/com/example/repositoryfinder/app/AppService.java
+++ src/main/java/com/example/repositoryfinder/app/AppService.java
@@ -1,13 +1,13 @@
 package com.example.repositoryfinder.app;
 
+import com.example.repositoryfinder.exception.UserNotFoundException;
 import com.example.repositoryfinder.model.Repo;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class AppService {
@@ -18,17 +18,30 @@ public class AppService {
         Map<String, Object> repositories = new LinkedHashMap<>();
         repositories.put("Username",username);
         List<Repo> reposFromAPI = getFromExternalAPI(username);
-        repositories.put("Stars","10");
+        if (reposFromAPI != null) {
+            repositories.put("Total stars",reposFromAPI.stream().map(Repo::getStars_count).reduce(0,Integer::sum));
+        }
         repositories.put("Repositories",reposFromAPI);
-
         return repositories;
     }
 
     private List<Repo> getFromExternalAPI(String username){
-        List<Repo> repos = new ArrayList<>();
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject("https://api.github.com/users/"+username+"/repos",String.class);
-        repos.add(new Repo("XDDD",10));
-        return repos;
+        try{
+            ResponseEntity<JsonNode[]> responseEntity =
+                    restTemplate.getForEntity(String.format("https://api.github.com/users/%s/repos", username)
+                            ,JsonNode[].class);
+            JsonNode[] objects = responseEntity.getBody();
+            if(objects!=null){
+                return Arrays.stream(objects)
+                        .map(x-> new Repo(x.get("name").asText(),x.get("stargazers_count").asInt()))
+                        .collect(Collectors.toList());
+            }else{
+                return null;
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            throw new UserNotFoundException();
+        }
     }
 }
